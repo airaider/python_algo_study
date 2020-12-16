ed3094b6b7a504f737adb9dc2a5d7377f66f6664
HongKiHwan <43366742+airaider@users.noreply.github.com>
2020-12-16 18:01:00 +0900

    day 3

---
+++ 6장_문자열 조작/그룹 애너그램.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/group-anagrams/
+
+#딕셔너리 자료형
+import collections
+def solution(strs: list[str]):
+    print(strs)
+    anagrams = collections.defaultdict(list)
+    for str in strs:
+        anagrams[''.join(sorted(str))].append(str)
+    print(anagrams.values())
+
+
+
+if __name__ == '__main__':
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    solution(strs)
--- python_tip.md
+++ python_tip.md
@@ -5,4 +5,17 @@
     하지만 가독성, 효율성을 위해서 함수 인자로 넘길 시에, 명시하는 것이 중요!
     
     ex) `def solution(test : int): print(test)` 
+
+
+2. `sorted()` 
+
+   tim 소트
+   
+   key= 옵션 가능
+
+   `.sort() `
+
+   in-place 소트
+
+   리턴값 없음 (None 리턴)
         
