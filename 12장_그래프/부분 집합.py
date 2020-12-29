fbbf898e4e44fa6d6cbe7f18a565962e2f7aeab4
HongKiHwan <43366742+airaider@users.noreply.github.com>
2020-12-29 22:40:07 +0900

    day 11
    sub-sums

---
+++ 12장_그래프/부분 집합.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/combination-sum/submissions/
+
+# 모든 원소 거치고 나온 다음에 append
+def solution(nums):
+    answer = []
+
+    def dfs(path, idx):
+        if idx == len(nums):
+            answer.append(path[:])
+            return
+        dfs(path+[nums[idx]], idx+1)
+        dfs(path, idx+1)
+    path = []
+    dfs(path, 0)
+    print(answer)
+
+
+# 매 iteration 나오는대로 append
+def solution1(nums):
+    answer = []
+
+    def dfs(path, idx):
+        answer.append(path[:])
+        for i in range(idx, len(nums)):
+            dfs(path+[nums[i]], i+1)
+    path = []
+    dfs(path, 0)
+    print(answer)
+
+
+if __name__ == '__main__':
+    nums = [1,2,3]
+    solution1(nums)
+
---
+++ 12장_그래프/조합의 합.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/combination-sum/submissions/
+
+# Nested function, DFS
+def solution(candidates, target):
+    answer = []
+
+    def dfs(sum, path, idx):
+        if sum>target:
+            return
+        elif sum==target:
+            answer.append(path[:])
+        else:
+            for i in range(idx, len(candidates)):
+                dfs(sum + candidates[i], path+[candidates[i]], i)
+
+    path = []
+    dfs(0, path, 0)
+    print(answer)
+
+
+if __name__ == '__main__':
+    candidates = [2,3,6,7]
+    target = 7
+    solution(candidates, target)
+
