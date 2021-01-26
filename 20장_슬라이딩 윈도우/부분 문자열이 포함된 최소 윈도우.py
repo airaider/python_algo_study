8bc1742f90d81077dbf63f73598fe4b1f578626a
HongKiHwan <43366742+airaider@users.noreply.github.com>
2021-01-27 00:29:00 +0900

    day 27

--- 20장_슬라이딩 윈도우/부분 문자열이 포함된 최소 윈도우.py
+++ 20장_슬라이딩 윈도우/부분 문자열이 포함된 최소 윈도우.py
@@ -5,20 +5,26 @@ def solution(s,t):
     need = collections.Counter(t)
     missing = len(t)
 
-    left = start = end = 0
-
     print(need)
     print(missing)
+
+    left = start = end = 0
+
     for right, char in enumerate(s, 1):
 
         missing -= need[char] > 0
+        need[char]-=1
 
-        # need[char]-=1
-        # if missing == 0:
-        #     while left<right and need[s[left]]<0:
-        #         need[s[left]]+=0
-        #         left+=1
-
+        if missing == 0:
+            while left<right and need[s[left]]<0:
+                need[s[left]]+=1
+                left+=1
+            if not end or right-left <= end-start:
+                start, end = left, right
+                need[s[left]] += 1
+                left += 1
+                missing+=1
+    print(s[start:end])
     return
 
 if __name__ == '__main__':
