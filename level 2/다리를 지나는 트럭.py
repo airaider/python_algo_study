98b7045c2144c59452f16dabf07b98181c56c949
HongKiHwan <43366742+airaider@users.noreply.github.com>
2021-02-08 22:23:24 +0900

    day 35

---
+++ level 2/다리를 지나는 트럭.py
@@ -0,0 +1,23 @@
+import collections
+
+def solution(bridge_length, weight, truck_weights):
+    time=0
+    bridge = [0]*bridge_length
+
+    while len(bridge)>0:
+        time+=1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge)+truck_weights[0]<=weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+    print(time)
+    return time
+
+
+if __name__ == '__main__':
+    truck_weights = [7, 4, 5, 6]
+    bridge_length = 2
+    weight = 10
+    solution(bridge_length, weight, truck_weights)
---
+++ level 2/삼각 달팽이.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+
+
+    return answer
+
+
+if __name__ == '__main__':
+    n = 4
+    solution(n)
