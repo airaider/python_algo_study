9cd8b9d891561ba010c6889aae78e61a6d178288
211959 <kh_hong@ssg.com>
2024-06-28 11:08:38 +0900

    해시: 완주하지 못한 선수 2

--- programmers/해시/완주하지 못한 선수.py
+++ programmers/해시/완주하지 못한 선수.py
@@ -2,11 +2,20 @@ from programmers.util.test_runner import run_tests
 import collections
 
 
-def solution(participant, completion):
+def solution1(participant, completion):
     a = collections.Counter(participant)
     b = collections.Counter(completion)
     c = a - b
     return list(c.keys())[0]
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i,e in enumerate(completion):
+        if e != participant[i]:
+            return participant[i]
+    return participant[-1]
 
 
 if __name__ == "__main__":
