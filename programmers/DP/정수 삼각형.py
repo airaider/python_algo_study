b2f9a3d932145966377beda8f5b9a5b27dd97db5
airaider <airaider@naver.com>
2024-08-19 21:03:41 +0900

    level3 dp

---
+++ programmers/DP/정수 삼각형.py
@@ -0,0 +1,18 @@
+def solution(triangle):
+    for i in range(len(triangle)):
+        if i == 0: continue
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+    print(max(triangle[-1]))
+    return
+
+
+if __name__ == '__main__':
+    triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+    result = 30
+    solution(triangle)
